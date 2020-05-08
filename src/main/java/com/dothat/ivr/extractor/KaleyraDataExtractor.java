e47bd545012b0f82cbb42a15932a85effe4bae79
Abhideep Singh <abhi@dothat.in>
2020-05-08 17:28:14 +0530

    Fix Kaleyra Data extractor based on Generic Extractor

--- src/main/java/com/dothat/ivr/extractor/KaleyraDataExtractor.java
+++ src/main/java/com/dothat/ivr/extractor/KaleyraDataExtractor.java
@@ -1,11 +1,13 @@
 package com.dothat.ivr.extractor;
 
+import com.dothat.ivr.data.IVRCall;
 import com.dothat.ivr.data.IVRDataField;
 import com.dothat.ivr.data.IVRProvider;
 import com.dothat.location.data.Country;
 import com.dothat.location.data.State;
 import com.dothat.location.impl.IndiaState;
 import com.dothat.validator.phone.CountryCode;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,19 +17,20 @@ import java.util.Map;
  *
  * @author abhideep@ (Abhideep Singh)
  */
-public class KaleyraDataExtractor {
+public class KaleyraDataExtractor extends GenericIVRDataExtractor {
   
   private final Map<IVRDataField, String> fieldNameMap = new HashMap<>();
   private final Map<String, Country> countryCodeMap = new HashMap<>();
   private final Map<String, State<?>> stateCodeMap = new HashMap<>();
   
   public KaleyraDataExtractor() {
+    super(IVRProvider.KALEYRA);
     fieldNameMap.put(IVRDataField.CALLER_NUMBER, "caller");
     fieldNameMap.put(IVRDataField.IVR_NUMBER, "called");
     fieldNameMap.put(IVRDataField.VIRTUAL_NUMBER, "dialed");
     fieldNameMap.put(IVRDataField.CALL_ID, "id");
     fieldNameMap.put(IVRDataField.COUNTRY, "country");
-    fieldNameMap.put(IVRDataField.STATE, "state");
+    fieldNameMap.put(IVRDataField.CIRCLE, "circle");
     fieldNameMap.put(IVRDataField.CALL_TIMESTAMP, "calltime");
     fieldNameMap.put(IVRDataField.CALL_DURATION, "duration");
     
@@ -37,13 +40,34 @@ public class KaleyraDataExtractor {
     stateCodeMap.put("HR", IndiaState.HARYANA);
   }
   
+  @Override
+  protected Map<IVRDataField, String> getFieldNameMap() {
+    return fieldNameMap;
+  }
+  
+  @Override
+  protected Map<String, Country> getCountryCodeMap() {
+    return countryCodeMap;
+  }
+  
+  @Override
+  protected Map<String, State<?>> getStateCodeMap() {
+    return stateCodeMap;
+  }
+  
+  @Override
+  public IVRCall extractCallData(String uri, JSONObject json) {
+    IVRCall data = super.extractCallData(uri, json);
+    // Set Call Timestamp
+    
+    return data;
+  }
+  
   public static class Factory implements IVRDataExtractor.Factory {
   
     @Override
     public IVRDataExtractor create() {
-      KaleyraDataExtractor extractor = new KaleyraDataExtractor();
-      return new GenericIVRDataExtractor(IVRProvider.KALEYRA, extractor.fieldNameMap,
-          extractor.countryCodeMap, extractor.stateCodeMap);
+      return new KaleyraDataExtractor();
     }
   }
 }
--- src/main/java/com/dothat/validator/PhoneNumberSanitizer.java
+++ src/main/java/com/dothat/validator/PhoneNumberSanitizer.java
@@ -18,15 +18,22 @@ public class PhoneNumberSanitizer {
   }
   
   public String sanitize(String number) {
+    // Remove all non digits
     String phone = number.replaceAll("[^0-9]", "");
+
+    // Remove all leading zeros
+    phone = phone.replaceAll("^[0]+", "");
     
     CountryCode code = CountryCodeLookup.getInstance().getCode(country);
     String countryCode = code.getCode();
     PhoneFormat format = code.getFormat();
-    
+
+    // Remove country code from the beginning
     if (phone.startsWith(countryCode) && phone.length() > format.getMaxDigits()) {
       phone = phone.substring(countryCode.length());
     }
+
+    // Add country code in the correct format
     phone = "+" + countryCode + "-" + phone;
     return phone;
   }
