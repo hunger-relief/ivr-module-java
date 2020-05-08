ca06c346545836c03e17c192970277759d32bd73
Abhideep Singh <abhi@dothat.in>
2020-05-08 11:34:52 +0530

    Generic Phone Number Sanitizer

---
+++ src/main/java/com/dothat/validator/phone/PhoneFormat.java
@@ -0,0 +1,28 @@
+package com.dothat.validator.phone;
+
+/**
+ * Data for format used by the country.
+ *
+ * @author abhideep@ (Abhideep Singh)
+ */
+public class PhoneFormat {
+  private final int minDigits;
+  private final int maxDigits;
+  
+  public PhoneFormat(int numDigits) {
+    this(numDigits, numDigits);
+  }
+  
+  public PhoneFormat(int minDigits, int maxDigits) {
+    this.minDigits = minDigits;
+    this.maxDigits = maxDigits;
+  }
+  
+  public final int getMinDigits() {
+    return minDigits;
+  }
+  
+  public final int getMaxDigits() {
+    return maxDigits;
+  }
+}
