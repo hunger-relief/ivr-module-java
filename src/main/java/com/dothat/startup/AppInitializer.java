4cf5626b4636133ce9662f95e6f3c1c409ea79b1
Abhideep Singh <abhi@dothat.in>
2020-05-09 06:41:16 +0530

    Initialize Objectify to use Local Datastore when testing locally

--- src/main/java/com/dothat/startup/AppInitializer.java
+++ src/main/java/com/dothat/startup/AppInitializer.java
@@ -1,17 +1,44 @@
 package com.dothat.startup;
 
+import com.dothat.objectify.PersistenceService;
+import com.google.appengine.api.utils.SystemProperty;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class AppInitializer implements ServletContextListener {
-  
+
+  private static final Logger logger = LoggerFactory.getLogger(AppInitializer.class);
+  private static final String LOCAL_APP_ID = "no_app_id";
+
   @Override
   public void contextInitialized(ServletContextEvent event) {
-    ObjectifyService.init();
+    String applicationId = SystemProperty.applicationId.get();
+    logger.warn("#### Running with Application Id {} ####", applicationId);
+    if (LOCAL_APP_ID.equals(applicationId)) {
+      initLocal();
+    } else {
+      ObjectifyService.init();
+    }
   }
   
+  private void initLocal() {
+    // Assumes that you are running a local data store by running the following command
+    // gcloud beta emulators datastore start --host-port=localhost:8484
+    ObjectifyService.init(new ObjectifyFactory(
+        DatastoreOptions.newBuilder()
+            .setHost("http://localhost:8484")
+            .setProjectId("hunger-relief-dev")
+            .build()
+            .getService()
+    ));
+  }
+
   @Override
   public void contextDestroyed(ServletContextEvent sce) {
     // Perform Cleanup
