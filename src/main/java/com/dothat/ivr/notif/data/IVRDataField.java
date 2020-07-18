713a3941b8dbff75aac4631731da327c44222a1a
Abhideep Singh <abhideep@>
2020-07-18 10:59:32 +0530

    Add Relay Urgency and Receiver Number is Call Log Webhook

--- src/main/java/com/dothat/ivr/notif/data/IVRDataField.java
+++ src/main/java/com/dothat/ivr/notif/data/IVRDataField.java
@@ -11,9 +11,11 @@ public enum IVRDataField implements Field {
   CIRCLE,
   LOCATION_HINT,
   
-  IVR_NUMBER,
-  DIALED_NUMBER,
+  IVR_NUMBER, // The number for the Bridge or IVR
+  DIALED_NUMBER, // The number that was dialed by the caller
+  RECEIVER_NUMBER, // The number of the agent who received the call
 
+  URGENCY,
   KEY_PRESS,
   
   CALL_TIMESTAMP,
